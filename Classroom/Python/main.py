fc06529eb3f988d0a523820dbedb96228df3b1b1
322899 <theprinceofallducks@gmail.com>
2024-09-18 11:49:54 -0400

    pygame

--- Classroom/Python/main.py
+++ Classroom/Python/main.py
@@ -1,8 +1,18 @@
 #Ryan
 #9/17/24
+import pygame
+
+WIDTH, HEIGHT = 900, 500
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
 def main():
-    pass
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
 
-if __name__ == '__main__':
+    pygame.quit()
+
+if __name__ == "__main__":
     main()
